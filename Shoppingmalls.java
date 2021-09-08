37003edc737372baf180d974989fc225563a78d4
Deepika S <deepika.xworkz@gmail.com>
2021-09-08 14:47:26 +0530

    added demo file

---
+++ Shoppingmalls.java
@@ -0,0 +1,20 @@
+class Shoppingmalls{
+
+ public static void main(String a[]){
+
+String[] shoppingmallsName = {"Mantrisquaremall","GT World","Westgate Value","Orion","Garuda","Global","Aishwarya"};
+  
+   // int[]noOfMalls = {7};
+ 
+ //System.out.println(shoppingmallsName[0]+" "+shoppingmallsName[1]+" "+shoppingmallsName[2]+" "+shoppingmallsName[3]+" "+shoppingmallsName[4]+" "+shoppingmallsName[5]+" "+shoppingmallsName[6] );
+// System.out.println(noOfMalls);
+  
+System.out.println(shoppingmallsName[0]);
+System.out.println(shoppingmallsName[1]);
+System.out.println(shoppingmallsName[2]);
+System.out.println(shoppingmallsName[3]);
+System.out.println(shoppingmallsName[4]);
+System.out.println(shoppingmallsName[5]);
+System.out.println(shoppingmallsName[6]);
+}
+}
