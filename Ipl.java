3c6a03a34e65c8802adfa39d5fab0f72efe672bc
Deepika S <deepika.xworkz@gmail.com>
2021-09-08 14:42:37 +0530

    added demo file

---
+++ Ipl.java
@@ -0,0 +1,25 @@
+class Ipl{
+static String Iplteams[] = {"Rcb","Srh","Mi","Rr","Kkr"};
+  
+ public static void main(String a[]){
+
+for(int s=0;s<Iplteams.length;s++){
+System.out.println(Iplteams[s]);
+  
+   // int[]noOfindianplayers = {4,6,5,7,3};
+ 
+// System.out.println(IplteamName[0]+" "+IplteamName[1]+" "+IplteamName[2]+" "+IplteamName[3]+" "+IplteamName[4] );
+// System.out.println(noOfindianplayers[0]+" "+noOfindianplayers[1]+" "+noOfindianplayers[2]+" "+noOfindianplayers[3]+" "+noOfindianplayers[4] );
+
+//System.out.println(IplteamName[0]);
+//System.out.println(IplteamName[1]);
+//System.out.println(IplteamName[2]);
+//System.out.println(IplteamName[3]);
+//System.out.println(IplteamName[4]);
+//System.out.println(noOfindianplayers[0]);
+//System.out.println(noOfindianplayers[1]);
+//System.out.println(noOfindianplayers[2]);
+//System.out.println(noOfindianplayers[3]);
+}
+}
+}
