c3639e0802a156b799d47b2de89436ff21289003
Deepika S <deepika.xworkz@gmail.com>
2021-09-08 14:44:38 +0530

    added demo file

---
+++ Sports.java
@@ -0,0 +1,11 @@
+class Sports
+{
+public static void main(String []args){
+ String SportsName="kabaddi";
+ String place="tumkur";
+ int totalnumber=7;
+System.out.println(SportsName);
+System.out.println(place);
+System.out.println(totalnumber);
+}
+}
